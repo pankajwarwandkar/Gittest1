29b7da008b21674f9c660c91c5d367697fb06aec
pankajwarwandkar <pankaj61190@gmail.com>
2018-10-24 22:43:12 +0530

    new file

---
+++ src/test/java/demo1/driver/Git12.java
@@ -0,0 +1,29 @@
+package demo1.driver;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+public class Git12 {
+
+	
+	@Test
+	public static void readData() {
+		
+		
+	
+	
+	ArrayList<String> arr= new ArrayList<String>();
+	
+	arr.add("Pankaj.w");
+	arr.add("Rahul");
+	
+	for(String a:arr) {
+	System.out.println(a);
+	
+	
+	}
+	}
+	
+	
+}
